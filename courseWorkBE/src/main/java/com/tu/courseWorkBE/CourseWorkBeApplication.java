ca41ab91d83fd7940bcf858f5d4ba680a99660bc
i342680 <dimo.georgiev@sap.com>
2019-03-18 14:29:38 +0200

    Spring initializr

---
+++ courseWorkBE/src/main/java/com/tu/courseWorkBE/CourseWorkBeApplication.java
@@ -0,0 +1,13 @@
+package com.tu.courseWorkBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkBeApplication.class, args);
+	}
+
+}
